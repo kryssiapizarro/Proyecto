22e84ec26fe7342fec5117ed962daff033daaf2e
Kry Pizarro♡ <kryssiavp@gmail.com>
2022-02-07 17:01:48 -0600

    prueba repos

--- src/Controlador/Arbol_ListaEspera.java
+++ src/Controlador/Arbol_ListaEspera.java
@@ -4,6 +4,6 @@ package Controlador;
 //esto es para poner los pacientes en lsta de espera si no hay ningun médico disponible
 //apenas se desocupe un médico se le asigna el paciente de la lista de espera segpun el orden de llegada
 public class Arbol_ListaEspera {
-
+//prueba repositorio
     
 }
