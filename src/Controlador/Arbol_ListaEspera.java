c1972b53d2ee05866b07a520d9ae465fe3ee2d24
Anthony Ortega <ajortega2290@gmail.com>
2022-02-17 16:24:25 -0600

    Desocupar no funciona porque la raiz es nula

--- src/Controlador/Arbol_ListaEspera.java
+++ src/Controlador/Arbol_ListaEspera.java
@@ -4,8 +4,10 @@ package Controlador;
 //esto es para poner los pacientes en lsta de espera si no hay ningun médico disponible
 
 import Modelo.Mascotas;
+import Modelo.NodoConsultorio;
 import Modelo.NodoListaEspera;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 //apenas se desocupe un médico se le asigna el paciente de la lista de espera segpun el orden de llegada
 public class Arbol_ListaEspera {
@@ -13,7 +15,7 @@ public class Arbol_ListaEspera {
        List listaDeEspera = new ArrayList<NodoListaEspera>();
 
     public Arbol_ListaEspera() {
-        raiz = null;
+        //raiz = null;
     }
     public boolean estaVacio(){
         return raiz == null;
@@ -54,7 +56,86 @@ public class Arbol_ListaEspera {
         }
     }
     
+    public Mascotas eliminar(NodoListaEspera auxiliar,NodoConsultorio nodo){
+        NodoListaEspera padre = auxiliar;
+        Mascotas mascota = new Mascotas();
+        boolean esHijoIzquierdo = true;
+        while(auxiliar.getCodigoDelPaciente()!= nodo.getDato().getMascota().getCodPaciente()){
+            padre = auxiliar;
+            mascota = auxiliar.getDato();
+            if(nodo.getDato().getMascota().getCodPaciente()<auxiliar.getDato().getCodPaciente()){
+                esHijoIzquierdo = true;
+                auxiliar = auxiliar.hijoIzquierdo;
+                mascota = auxiliar.getDato();
+            }else{
+                esHijoIzquierdo = false;
+                auxiliar = auxiliar.hijoDerecho;
+                mascota = auxiliar.getDato();
+            }
+            if(auxiliar == null){
+                JOptionPane.showMessageDialog(null, "No hay nada");
+            }
+        }//fin del while
+        if(auxiliar.hijoIzquierdo == null && auxiliar.hijoDerecho == null){
+            if(auxiliar == raiz){
+                raiz = null;
+            }else if(esHijoIzquierdo){
+                padre.hijoIzquierdo = null;
+            }else{
+                padre.hijoDerecho = null;
+            }
+        }else if(auxiliar.hijoDerecho == null){
+            if(auxiliar == raiz){
+                raiz = auxiliar.hijoIzquierdo;
+                mascota = raiz.getDato();
+            }else if(esHijoIzquierdo){
+                padre.hijoIzquierdo = auxiliar.hijoIzquierdo;
+                mascota = auxiliar.hijoIzquierdo.getDato();
+            }else{
+                padre.hijoDerecho = auxiliar.hijoIzquierdo;
+                mascota = auxiliar.hijoIzquierdo.getDato();
+            }
+        }else if(auxiliar.hijoIzquierdo == null){
+            if(auxiliar == raiz){
+                raiz = auxiliar.hijoDerecho;
+                mascota = raiz.getDato();
+            }else if(esHijoIzquierdo){
+                padre.hijoDerecho = auxiliar.hijoDerecho;
+                mascota = padre.hijoIzquierdo.getDato();
+            }else{
+                padre.hijoIzquierdo = auxiliar.hijoDerecho;
+                mascota = padre.hijoDerecho.getDato();
+            }
+        }else{
+            NodoListaEspera reemplazo = obtenerNodoReemplazo(auxiliar);
+            if(auxiliar == raiz){
+                raiz = reemplazo;
+            }else if(esHijoIzquierdo){
+                padre.hijoIzquierdo = reemplazo;
+            }else{
+                padre.hijoDerecho = reemplazo;
+            }
+            reemplazo.hijoIzquierdo = auxiliar.hijoIzquierdo;
+        }
+        return mascota;
+    }
     
+    public NodoListaEspera obtenerNodoReemplazo(NodoListaEspera nodoReemplazo){
+        NodoListaEspera reemplazarPadre = nodoReemplazo;
+        NodoListaEspera reemplazo = nodoReemplazo;
+        NodoListaEspera auxiliar = nodoReemplazo.hijoDerecho;
+        while(auxiliar != null){
+            reemplazarPadre = reemplazo;
+            reemplazo = auxiliar;
+            auxiliar = auxiliar.hijoIzquierdo;
+        }
+        if(reemplazo != nodoReemplazo.hijoDerecho){
+            reemplazarPadre.hijoIzquierdo = reemplazo.hijoDerecho;
+            reemplazo.hijoDerecho = nodoReemplazo.hijoDerecho;
+        }
+        System.out.println("El nodo reemplazo es: "+reemplazo);
+        return reemplazo;
+    }
 //prueba repositorio
 
     @Override
